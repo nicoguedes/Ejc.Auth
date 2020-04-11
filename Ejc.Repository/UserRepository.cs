4a201d35a14c3cbe4b5e008f1ffa88b285af2b3e
Vinícius Guedes <viniciusmpg@gmail.com>
2020-04-10 23:07:05 -0300

    Migrating from Node API to dotnet core

--- Ejc.Repository/UserRepository.cs
+++ Ejc.Repository/UserRepository.cs
@@ -14,6 +14,7 @@ namespace Ejc.Repository
         public UserRepository(IConfiguration config) : base(config) { }
         public User Authenticate(string email, string password)
         {
+            // TODO: Password MD5 hash
             return _collection.Find<User>(o => o.Email == email && o.Password == password).FirstOrDefault();
         }
     }
